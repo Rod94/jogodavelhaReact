0dafa0cdf1784e0aa080ec02a98bc3cddc8375ca
Rodolpho Caetano <rphfti@hotmail.com>
2020-12-16 09:54:26 -0300

    texto SOBRE adicionado

--- src/App.js
+++ src/App.js
@@ -3,20 +3,13 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="sobre">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+
         <p>
-          Edite <code>src/App.js</code> e salve para reload.
+          Sobre
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          React
-        </a>
+
       </header>
     </div>
   );
