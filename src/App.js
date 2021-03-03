a3f010b56c0feb00a67ba41e0f3aff80d6d58ffe
Rodolpho Caetano <rphfti@hotmail.com>
2021-03-03 20:17:19 -0300

    criada pasta logo e inserido na tela

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './css/generic/reset.css'
 import './css/settings/colors.css';
 import Sobre from './objects/sobre';
+import Logo from './objects/logo';
 
 function App() {
   return (
-    <Sobre />
+    <>
+      <Sobre />
+      <Logo />
+    </>
   )
 }
 
---
+++ src/objects/logo/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './styles.css';
+import discoElysium from '../../assets/logo.png';
+
+const Logo = () => {
+  return (<img className="logo" src={discoElysium} alt="Logo da Disco Elysium" />)
+}
+
+export default Logo;
