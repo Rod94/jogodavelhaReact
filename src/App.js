a2b18e47bd2eb233c9bce13b2dd63c907735c466
Rodolpho Caetano <rphfti@hotmail.com>
2021-03-08 17:47:04 -0300

    menu hamburguer finalizado

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './css/generic/reset.css'
 import './css/settings/colors.css';
 import Sobre from './objects/sobre';
 import Logo from './objects/logo';
+import Menu from './objects/menu';
 
 function App() {
   return (
     <>
       <Sobre />
       <Logo />
+      <Menu />
     </>
   )
 }
---
+++ src/objects/menu/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles.css'
+
+const Menu = () => {
+  return (  
+  <>
+    <span className="hamburguer" />
+    <span className="hamburguer" />
+    <span className="hamburguer" />
+  </>
+  );
+}
+
+export default Menu;
