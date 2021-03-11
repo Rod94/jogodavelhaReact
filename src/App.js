b25c81864e2afdb52d5795e416fd6eaba4e5de38
Rodolpho Caetano <rphfti@hotmail.com>
2021-03-11 19:05:37 -0300

    add player XO e refat board

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './css/generic/reset.css'
 import './css/settings/colors.css';
 import Header from './components/header';
 import Card from './objects/card';
+import Player from './objects/player';
 
 function App() {
   return (
     <>
       <Header />
       <Card />
+      <Player />
     </>
   )
 }
---
+++ src/objects/player/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './styles.css';
+import X from '../../assets/X.png';
+import Circle from '../../assets/Circle.png';
+
+const Player = () => {
+  return (
+    <>
+      <img src={X}></img>
+      <img src={Circle}></img>
+    </>
+  )
+}
+
+
+export default Player;
