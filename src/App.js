0a3b8a4d0c7aeba902d52ecf2f470ca884652b4c
Rodolpho Caetano <rphfti@hotmail.com>
2021-03-11 18:46:56 -0300

    feito o board

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './css/generic/reset.css'
 import './css/settings/colors.css';
 import Header from './components/header';
+import Card from './objects/card';
 
 function App() {
   return (
     <>
       <Header />
+      <Card />
     </>
   )
 }
